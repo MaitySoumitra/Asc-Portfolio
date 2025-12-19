2405c9675dbd780f78990555c9af2d63104c3d4e
MaitySoumitra <soumitra.ahaansoftware@gmail.com>
2025-12-19 17:59:42 +0530

    Enhance CORS configuration in server.js

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(cors({
+    origin: process.env.SERVER_CORS,  
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,  
+}));
 
 app.use('/api/ui', uiRoutes);
 app.use('/api/auth', authRoutes);
